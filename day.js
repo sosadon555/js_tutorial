1cf79e0389ce45692a13c169bbcc698921e29dbc
ml <m.lietava@hotmail.com>
2019-08-15 19:54:23 +0100

    Add some test scripts

--- day.js
+++
@@ -1,8 +0,0 @@
-//Returns day of the week for the date
-function dayName(date) {
-  const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
-                        "Friday", "Saturday"];
-  return daysOfTheWeek[date.getDay()];
-}
-
-//Returns greeting for the given date
---
+++ objects.js
@@ -0,0 +1,11 @@
+var person = {
+  firstName:"Bob",
+  lastName:"Brown",
+  age:22
+};
+
+
+function fullName() {
+    return person.firstName + " " + person.lastName;
+  };
+console.log(fullName());
